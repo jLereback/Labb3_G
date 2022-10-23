8ea3b0b990e7077195f3431b0297f5138d66b964
julia <julia.lereback@gmail.com>
2022-10-23 09:29:34 +0200

    Add resize in ResizablePaintingArea

--- src/main/java/se/iths/MainApplication.java
+++ src/main/java/se/iths/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        Scene scene = new Scene(fxmlLoader.load(), 550, 450);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/se/iths/ResizablePaintingArea.java
+++ src/main/java/se/iths/ResizablePaintingArea.java
@@ -22,11 +22,59 @@ public class ResizablePaintingArea extends Canvas {
      * @see #maxHeight(double)
      *
      * @return orientation of width/height dependency or null if there is none
+     *
+     *
+     * #resize(double, double)
+     * Invoked by the region's parent during layout to set the region's width and height.
+     * Applications should not invoke this method directly.
+     * If an application needs to directly set the size of the region,
+     * it should override its size constraints by calling setMinSize(), setPrefSize(), or setMaxSize()
+     * and it's parent will honor those overrides during layout.
+     * Params:
+     * width – the target layout bounds width
+     * height – the target layout bounds height
      */
+
+
     @Override
     public boolean isResizable() {
         return true;
     }
 
+    @Override
+    public double minWidth(double height) {
+        return 350;
+    }
 
+    @Override
+    public double minHeight(double width) {
+        return 240;
+    }
+
+    @Override
+    public double prefWidth(double height) {
+        return 550;
+    }
+
+    @Override
+    public double prefHeight(double width) {
+        return 390;
+    }
+
+    @Override
+    public double maxWidth(double height) {
+        return Double.MAX_VALUE;
+    }
+
+
+    @Override
+    public double maxHeight(double width) {
+        return Double.MAX_VALUE;
+    }
+
+    @Override
+    public void resize(double width, double height) {
+        this.setWidth(width);
+        this.setHeight(height);
+    }
 }
